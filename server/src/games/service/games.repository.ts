97194939f76368f0defa56c3b64fc4ef7ebd166b
yagoFogaca <yagofogaca20@gmail.com>
2023-05-11 20:59:32 -0300

    feat: criação do repository de jogos

---
+++ server/src/games/service/games.repository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { IGamesEntity } from '../entities/index.entities';
+import { PrismaService } from 'src/database/database.service';
+
+@Injectable()
+export class GamesRepository {
+  constructor(private readonly prismaServe: PrismaService) {}
+
+  async create(game: IGamesEntity): Promise<IGamesEntity> {
+    return await this.prismaServe.games.create({ data: game });
+  }
+
+  async findAllTeams(id_team: string): Promise<IGamesEntity[]> {
+    return await this.prismaServe.games.findMany({
+      where: {
+        OR: [{ id_team_home: id_team }, { id_team_visiting: id_team }],
+      },
+    });
+  }
+
+  async delete(id: string): Promise<IGamesEntity> {
+    return await this.prismaServe.games.delete({ where: { id: id } });
+  }
+}
