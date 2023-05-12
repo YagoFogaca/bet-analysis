67f86ed5c93f6e90afb080630eb22498d2368219
yagoFogaca <yagofogaca20@gmail.com>
2023-05-12 14:54:23 -0300

    feat: criação do service

---
+++ server/src/games/service/games.service.ts
@@ -0,0 +1,25 @@
+import { GamesCreateUsecase } from './usecase/create.usecase';
+import { GameFindById } from './usecase/find-by-id.usecase';
+import { GameDeleteUsecase } from './usecase/delete.usecase';
+import { IGamesEntity } from '../entities/index.entities';
+import { GamesCreateDto } from '../dto/index.create-games-dto';
+
+export class GameService {
+  constructor(
+    private readonly createUsecase: GamesCreateUsecase,
+    private readonly findByIdUsecase: GameFindById,
+    private readonly deleteUsecase: GameDeleteUsecase,
+  ) {}
+
+  async create(game: GamesCreateDto): Promise<string> {
+    return await this.createUsecase.execute(game);
+  }
+
+  async findById(id: string): Promise<IGamesEntity> {
+    return await this.findByIdUsecase.execute(id);
+  }
+
+  async delete(id: string): Promise<string> {
+    return await this.deleteUsecase.execute(id);
+  }
+}
